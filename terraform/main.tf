71737fc9a8b8988da45d1e2fdd4e25e99608b93c
Ritika <rkumari.it.2017@nist.edu>
2026-07-22 18:18:48 +0530

    Provision Azure Resource Group using Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,8 @@
+
+resource "azurerm_resource_group" "rg" {
+
+  name     = "rg-healthconnect-dev"
+
+  location = "Central India"
+
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,5 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "d8523f2c-f2dd-47a8-a2b2-2197acdf1e54"
+}
